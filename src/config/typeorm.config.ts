ba971216d804ed2f1ee046c711a52abc839f2495
art-83 <arthurfmedeiros77@gmail.com>
2026-04-12 01:59:27 -0300

    feat: production/development database setup

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,20 @@
+import fs from 'fs';
+
 const typeOrmConfig = {
   type: process.env.DB_TYPE,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [__dirname + String(process.env.ORM_ENTITIES_PATH)],
+  ssl: {
+    ca: fs.readFileSync(__dirname + String(process.env.SSL_CERT_PATH)),
+  },
+  extra: {
+    max: 10,
+    idleTimeoutMillis: 30000,
+  }
 };
 
 export default typeOrmConfig;
