da0b6dd8af9a79d61507db37b34dc4f08786970f
art-83 <arthurfmedeiros77@gmail.com>
2026-04-12 01:14:10 -0300

    feat: better separation in development environment

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ const typeOrmConfig = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/../modules/**/infra/orm/entities/*.entity.js'],
+  entities: [__dirname + String(process.env.ORM_ENTITIES_PATH)],
 };
 
 export default typeOrmConfig;
