173c7de8d70cdcc4cee3e5d96148b8c602880114
art-83 <arthurfmedeiros77@gmail.com>
2026-04-12 03:25:27 -0300

    feat: routes indexed on entrypoint

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dataSource from '../orm/database';
+import routes from './routes';
 
 async function main() {
   const port = process.env.PORT;
@@ -7,6 +8,8 @@ async function main() {
   const app = express();
 
   app.use(express.json());
+  
+  app.use(routes);
 
   await dataSource.initialize();
 
