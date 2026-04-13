c1f6c2563c19211f99921a9be5e3b14088b1a89a
art-83 <arthurfmedeiros77@gmail.com>
2026-04-12 21:44:53 -0300

    feat: pre commit hook to run prettier formatation

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import globalErrorHandlerMiddleware from "../middlewares/global-error-handler.middleware";
 
 const routes = Router();
+
+routes.use(globalErrorHandlerMiddleware);
 
 routes.use('/health', (request, response) => {
   return response.status(200).json({ message: 'Strawberry fields forever!' });
 });
+
 
 export default routes;
